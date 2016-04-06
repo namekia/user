aea634fd93bc9662cf68f4401776e92ba562bf2d
Gregory Chambers <greg@gChambers.com>
2016-04-06 15:27:02 -0500

    log file

--- ___github-webrefresh.php
+++ ___github-webrefresh.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Chicago');
 ignore_user_abort(true);
 set_time_limit(0);
 
-$repo          = '~/public_html/';
+$repo          = '~/public_html/user';
 $branch        = 'master';
 $output        = array();
 
